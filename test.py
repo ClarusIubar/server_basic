9ca8cd1fa7551d4872cc0f991e04cbc37b76106b
ClarusIubar <iubar@duck.com>
2026-01-20 19:18:44 +0900

    problem 1,2

--- test.py
+++
@@ -1,9 +0,0 @@
-import socket 
-
-host_name = "localhost"
-addr = socket.getaddrinfo(host_name, None)
-
-for res in addr:
-    print(f"family: {res[0].name}, address: {res[4][0]}")
-    # family: AF_INET6, address: ::1
-    # family: AF_INET, address: 127.0.0.1
